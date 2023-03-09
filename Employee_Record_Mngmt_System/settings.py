b9a79b56b6b71afeb8eda957b3f35143e7898d83
@akashg12345 <akash.gautam29@gmail.com>
2023-03-09 12:23:57 +0530

    third commit 3

--- Employee_Record_Mngmt_System/settings.py
+++ Employee_Record_Mngmt_System/settings.py
@@ -100,7 +100,10 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+import os
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -158,4 +161,4 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ["*"]
