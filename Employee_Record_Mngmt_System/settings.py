1ae25c6197f946ca67169dc5838333bae8fee0ca
@akashg12345 <akash.gautam29@gmail.com>
2023-03-09 12:18:27 +0530

    third commit 2

--- Employee_Record_Mngmt_System/settings.py
+++ Employee_Record_Mngmt_System/settings.py
@@ -101,9 +101,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
